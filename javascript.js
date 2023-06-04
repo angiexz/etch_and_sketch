95c144f9970c4f05273b52667ee1e70ed6b7f477
angiexz <97621483+angiexz@users.noreply.github.com>
2023-06-04 14:32:33 -0400

    finished! Button fixed, new graphs can now be generated

--- javascript.js
+++ javascript.js
@@ -1,42 +1,58 @@
-//TODO
-//(1) Make box generation & event listener into a function
-//(2) Fix the button thing --> how to read input text? 
-//(3) Fix dimension calculation --> hhow to change width and height in css from javascript
 
-//Function to make the div 
-var dim, container, box; 
+var dim, container, box, boxes; 
 //Default value
 num = 16;
-
-//Function to generate new graph & add event listeners to the boxes
-dim = 960/num;
-//Create new div
-for(let i = 0;i<num**2;i++){
-    //Get parent div
-    container = document.getElementById('big_container');
-    //Create new HTML element
-    box = document.createElement('div');
-    box.id = 'square'
-    //Add HTML to DOM
-    container.appendChild(box);
-}
-
-//Get all boxes
-let squares=document.querySelectorAll('#square');
-Array.from('squares');
-
-//Add an event listener to every click
-for(let j = 0; j<num**2; j++){
-    let s = squares[j];
-    s.addEventListener('mouseover',()=>{
-        s.style.backgroundColor='black';
-    })
-}
+boxes = generateGraph(num);
 
 //Add an event listener to the button --> new graph is generated with every click
-let b = document.querySelector('e');
-let t = document.querySelector('num_box');
-b.addEventListener('onclick',()=>{
-    num = toString(t);
-    console.log(num);
+//will need to account for edge cases in the future --> entered a non number, or number > 100, or negative
+const b = document.querySelector('#e');
+let t = document.querySelector('#num_box');
+b.addEventListener('click',()=>{
+    num = t.value;
+    removePrevious(boxes);
+    boxes = generateGraph(num);
 })
+
+//remove elements from DOM so can create new graph
+function removePrevious(prev_squares){
+    let c = document.getElementById('big_container');
+    for (let s = 0 ; s<prev_squares.length;s++){
+        c.removeChild(prev_squares[s]);
+        console.log('slay');
+    }
+}
+
+ //Function to generate new graph & add event listeners to the boxes
+function generateGraph(n){
+    dim = 960/n;
+    //Create new div
+    for(let i = 0;i<n**2;i++){
+        //Get parent div
+        container = document.getElementById('big_container');
+        //Create new HTML element
+        box = document.createElement('div');
+        box.id = 'square'
+        //Add HTML to DOM
+        container.appendChild(box); 
+        //sizing
+        let sizing = dim+"px";
+        box.style.width = sizing;
+        box.style.height = sizing
+    }
+
+    //Get all boxes
+    let squares=document.querySelectorAll('#square');
+    Array.from('squares');
+
+    //Add an event listener to every square
+    for(let j = 0; j<n**2; j++){
+        let s = squares[j];
+        s.addEventListener('mouseover',()=>{
+        s.style.backgroundColor='black';
+        })
+    }
+    return squares;
+}
+
+
